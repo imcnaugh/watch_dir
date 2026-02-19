88d219459f6aaa8a0751ac430a4d8aafd46caa5b
Ian Mcnaughton <IanMcNaugh@gmail.com>
2026-02-18 23:06:33 -0600

    thats the folde watcher

--- dev_test_main/src/main.rs
+++ dev_test_main/src/main.rs
@@ -1,5 +1,14 @@
-use ed_log_scraper::watch_folder;
+use ed_log_scraper;
+use std::collections::HashSet;
+use std::path::PathBuf;
 
 fn main() {
-    watch_folder(".".to_string());
+    let path = PathBuf::from(".");
+    let extensions = HashSet::from(["log".to_string()]);
+    let handler = ed_log_scraper::folder_watcher::watch_folder_for_updates(&path, extensions);
+    let rx = handler.get_receiver();
+
+    for e in rx {
+        println!("{:?}", e);
+    }
 }
---
+++ ed_log_scraper/src/folder_watcher.rs
@@ -0,0 +1,58 @@
+use notify::EventKind;
+use notify::event::{DataChange, ModifyKind};
+use notify::{Event, RecursiveMode, Watcher};
+use std::collections::HashSet;
+use std::path::PathBuf;
+use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
+
+pub struct FolderWatchHandle {
+    _watcher: notify::RecommendedWatcher,
+    rx: Receiver<PathBuf>,
+}
+
+impl FolderWatchHandle {
+    pub fn get_receiver(&self) -> &Receiver<PathBuf> {
+        &self.rx
+    }
+}
+
+pub fn watch_folder_for_updates(
+    path: &PathBuf,
+    extensions_to_watch: HashSet<String>,
+) -> FolderWatchHandle {
+    let (notify_tx, notify_rx) = mpsc::channel::<notify::Result<Event>>();
+    let (tx, rx) = mpsc::channel::<PathBuf>();
+
+    let mut watcher = notify::recommended_watcher(notify_tx).unwrap();
+    watcher.watch(path, RecursiveMode::NonRecursive).unwrap();
+
+    std::thread::spawn(move || {
+        for event in notify_rx {
+            let evt = match event {
+                Ok(evt) => evt,
+                Err(e) => {
+                    eprintln!("watch error: {:?}", e);
+                    continue;
+                }
+            };
+
+            if let EventKind::Modify(ModifyKind::Data(DataChange::Content)) = evt.kind {
+                for path in evt.paths {
+                    if path
+                        .extension()
+                        .and_then(|ext| ext.to_str())
+                        .is_some_and(|ext_str| extensions_to_watch.contains(ext_str))
+                    {
+                        let _ = tx.send(path);
+                    }
+                }
+            }
+        }
+    });
+
+    FolderWatchHandle {
+        _watcher: watcher,
+        rx,
+    }
+}
