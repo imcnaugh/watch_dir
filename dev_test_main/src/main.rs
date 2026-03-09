4a1ef81b7bb612baf17ba3b7ab69a5ce49a58e14
Ian Mcnaughton <IanMcNaugh@gmail.com>
2026-03-08 20:57:42 -0500

    renaming log scraper to watch_dir

--- dev_test_main/src/main.rs
+++ dev_test_main/src/main.rs
@@ -1,4 +1,4 @@
-use ed_log_scraper::file_reader::ReadStrategy;
+use watch_dir::file_reader::ReadStrategy;
 use std::path::PathBuf;
 use std::sync::mpsc::Receiver;
 
@@ -6,8 +6,8 @@ const ED_LOG_PATH: &str = "C:\\Users\\Ian\\Saved Games\\Frontier Developments\\E
 const TEST_PATH: &str = "/Users/ian/Documents/code/ed_log_parser";
 
 fn main() {
-    let path = PathBuf::from(ED_LOG_PATH);
-    let mut idk = ed_log_scraper::file_reader::FileReader::new(&path, ed_read_strategy).unwrap();
+    let path = PathBuf::from(TEST_PATH);
+    let mut idk = watch_dir::file_reader::FileReader::new(&path, test_read_strategy).unwrap();
     let rx: Receiver<(PathBuf, String)> = idk.take_receiver().unwrap();
     for (p, c) in rx {
         if p.extension().unwrap().to_str().unwrap() == "log" {
@@ -17,6 +17,7 @@ fn main() {
 }
 
 fn ed_read_strategy(path: &PathBuf) -> ReadStrategy {
+    println!("{:?}", path);
     match path.extension().unwrap().to_str() {
         Some("log") => ReadStrategy::TailLines,
         Some("json") => ReadStrategy::Replace,
