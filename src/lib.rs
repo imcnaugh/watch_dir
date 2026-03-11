8b9b9dc194fd75a03c3092095d933fb5b6ab2cf6
Ian Mcnaughton <IanMcNaugh@gmail.com>
2026-03-10 22:18:33 -0500

    a quick todo

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod file_reader;
 mod folder_watcher;
 
+// TODO rename file reader, its a horrible name for the library to expose
 pub use file_reader::FileReader;
 pub use file_reader::ReadStrategy;
 pub use file_reader::{REPLACE_STRATEGY, TAIL_LINES_STRATEGY, TAIL_STRATEGY};
