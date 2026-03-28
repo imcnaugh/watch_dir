55e54d4c5bbeb70324df461f4174c32b2c002ceb
Ian Mcnaughton <IanMcNaugh@gmail.com>
2026-03-27 21:55:33 -0500

    more simple tests for tail lines strat, quite a lot of boilerplate is starting to appear

--- tests/tail_lines_stragety_test.rs
+++ tests/tail_lines_stragety_test.rs
@@ -1,4 +1,4 @@
-use crate::common::DEFAULT_WATCHER_DEBOUNCE_DURATION;
+use crate::common::{DEFAULT_CHANNEL_RECV_TIMEOUT, DEFAULT_WATCHER_DEBOUNCE_DURATION};
 use std::fs::File;
 use std::io::Write;
 use std::sync::mpsc::{RecvTimeoutError, TryRecvError};
@@ -21,7 +21,7 @@ fn tail_lines_strategy_simple_test() {
     drop(create_test_file_handle);
 
     assert!(matches!(
-        rx.recv_timeout(DEFAULT_WATCHER_DEBOUNCE_DURATION),
+        rx.recv_timeout(DEFAULT_CHANNEL_RECV_TIMEOUT),
         Err(RecvTimeoutError::Timeout)
     ));
 
@@ -29,13 +29,13 @@ fn tail_lines_strategy_simple_test() {
     test_file_handle.write_all(b"test\nmore text\n").unwrap();
     drop(test_file_handle);
 
-    let msg = rx.recv_timeout(DEFAULT_WATCHER_DEBOUNCE_DURATION).unwrap();
+    let msg = rx.recv_timeout(DEFAULT_CHANNEL_RECV_TIMEOUT).unwrap();
     assert_eq!(msg.1, "test");
     assert_eq!(
         test_file_path.canonicalize().unwrap(),
         msg.0.canonicalize().unwrap()
     );
-    let msg = rx.recv_timeout(DEFAULT_WATCHER_DEBOUNCE_DURATION).unwrap();
+    let msg = rx.recv_timeout(DEFAULT_CHANNEL_RECV_TIMEOUT).unwrap();
     assert_eq!(msg.1, "more text");
     assert_eq!(
         test_file_path.canonicalize().unwrap(),
@@ -49,7 +49,7 @@ fn tail_lines_strategy_simple_test() {
     drop(test_file_handle);
 
     assert!(matches!(
-        rx.recv_timeout(DEFAULT_WATCHER_DEBOUNCE_DURATION),
+        rx.recv_timeout(DEFAULT_CHANNEL_RECV_TIMEOUT),
         Err(RecvTimeoutError::Timeout)
     ));
 
@@ -57,7 +57,7 @@ fn tail_lines_strategy_simple_test() {
     test_file_handle.write_all(b" just yet\n").unwrap();
     drop(test_file_handle);
 
-    let msg = rx.recv_timeout(DEFAULT_WATCHER_DEBOUNCE_DURATION).unwrap();
+    let msg = rx.recv_timeout(DEFAULT_CHANNEL_RECV_TIMEOUT).unwrap();
     assert_eq!(msg.1, "not a complete line just yet");
     assert_eq!(
         test_file_path.canonicalize().unwrap(),
@@ -68,4 +68,85 @@ fn tail_lines_strategy_simple_test() {
 
     watcher.stop();
     assert_eq!(rx.try_recv(), Err(TryRecvError::Disconnected));
+}
+
+#[test]
+fn no_message_sent_for_empty_file() {
+    let dir = common::TestDir::new("no_message_sent_for_empty_file");
+    let test_file_path = dir.path().join("test_file.txt");
+
+    let options = watch_dir::Options::new()
+        .with_read_strategy_selector(TAIL_LINES_STRATEGY)
+        .with_notify_debounce_duration(DEFAULT_WATCHER_DEBOUNCE_DURATION);
+    let mut watcher = watch_dir::Watcher::new(dir.path(), options).unwrap();
+    let rx = watcher.take_receiver().unwrap();
+
+    let create_test_file_handle = File::create(&test_file_path).unwrap();
+    drop(create_test_file_handle);
+
+    assert_eq!(rx.try_recv(), Err(TryRecvError::Empty));
+
+    let mut test_file_handle = File::options().append(true).open(&test_file_path).unwrap();
+    test_file_handle.write(b"\n").unwrap();
+    drop(test_file_handle);
+
+    let msg = rx.recv_timeout(DEFAULT_CHANNEL_RECV_TIMEOUT);
+    assert!(matches!(msg, Err(RecvTimeoutError::Timeout)));
+
+    watcher.stop();
+}
+
+#[test]
+fn offset_is_reset_when_file_is_smaller_then_current_offset() {
+    let dir = common::TestDir::new("offset_is_reset_when_file_is_smaller_then_current_offset");
+    let test_file_path = dir.path().join("test_file.txt");
+
+    let options = watch_dir::Options::new()
+        .with_read_strategy_selector(TAIL_LINES_STRATEGY)
+        .with_notify_debounce_duration(DEFAULT_WATCHER_DEBOUNCE_DURATION);
+    let mut watcher = watch_dir::Watcher::new(dir.path(), options).unwrap();
+    let rx = watcher.take_receiver().unwrap();
+
+    let create_test_file_handle = File::create(&test_file_path).unwrap();
+    drop(create_test_file_handle);
+
+    assert_eq!(rx.try_recv(), Err(TryRecvError::Empty));
+
+    let mut test_file_handle = File::options().write(true).open(&test_file_path).unwrap();
+    test_file_handle
+        .write_all(b"quite a lot of text it just keeps going longer and longer\n\n\n")
+        .unwrap();
+    drop(test_file_handle);
+
+    let msg = rx.recv_timeout(DEFAULT_CHANNEL_RECV_TIMEOUT).unwrap();
+    assert_eq!(
+        msg.1,
+        "quite a lot of text it just keeps going longer and longer"
+    );
+    assert_eq!(
+        test_file_path.canonicalize().unwrap(),
+        msg.0.canonicalize().unwrap()
+    );
+    assert_eq!(rx.try_recv(), Err(TryRecvError::Empty));
+
+    let mut test_file_handle = File::options()
+        .truncate(true)
+        .write(true)
+        .open(&test_file_path)
+        .unwrap();
+    test_file_handle.write_all(b"truncated\n").unwrap();
+    drop(test_file_handle);
+
+    let msg = rx.recv_timeout(DEFAULT_CHANNEL_RECV_TIMEOUT).unwrap();
+    assert_eq!(msg.1, "truncated");
+    assert_eq!(
+        test_file_path.canonicalize().unwrap(),
+        msg.0.canonicalize().unwrap()
+    );
+    assert_eq!(rx.try_recv(), Err(TryRecvError::Empty));
+
+    let msg = rx.recv_timeout(DEFAULT_CHANNEL_RECV_TIMEOUT);
+    assert!(matches!(msg, Err(RecvTimeoutError::Timeout)));
+
+    watcher.stop();
 }
